37d4698d5d10a43f5bfdfbaa7dc960ffdcadf4a0
fangyixiao18 <fangyx18@hotmail.com>
2023-05-23 10:45:32 +0800

    fix bug of text input

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ os.system('python -m mim install mmcv')
 os.system('python -m mim install -e ".[multimodal]"')
 
 import gradio as gr
-from mmpretrain.apis import ImageClassificationInferencer
+from mmpretrain.apis import ImageClassificationInferencer, get_model
 from functools import partial
 
 
@@ -41,6 +41,7 @@ class ImageClassificationTab:
             'cn-clip_vit-large-p14_zeroshot-cls_cifar100',
         ]
         self.tab = self.create_ui()
+        self.current_text = None
 
     def create_ui(self):
         with gr.Row():
@@ -62,7 +63,9 @@ class ImageClassificationTab:
                 in_text = gr.Textbox(
                     value='鸟,猫,狗',
                     label='中文标签',
-                    info="请输入中文类别, 例如 '鸟,猫,狗', 请使用英文标点符号")
+                    info="请输入中文类别, 例如 '鸟,猫,狗'",
+                    interactive=True,
+                )
                 gr.Examples(
                     examples=[
                         os.path.join("examples", e)
@@ -81,6 +84,7 @@ class ImageClassificationTab:
                     'Run',
                     elem_classes='run_button',
                 )
+
                 run_button.click(
                     self.inference,
                     inputs=[select_model, in_image, in_text],
@@ -88,15 +92,22 @@ class ImageClassificationTab:
                 )
 
     def inference(self, model, image, text):
-        inferencer_name = self.__class__.__name__ + model
-        text = text.strip(' ').split(',')
+        clean_text = text.strip(' ').replace('，', ',')
+        inferencer_name = self.__class__.__name__ + model + clean_text
+
+        text_classes = clean_text.split(',')
+
+        if self.current_text is not None and self.current_text == clean_text:
+            pass
+        else:
+            model = get_model(
+                model, pretrained=True, text_prototype=text_classes)
+
         inferencer = InferencerCache.get_instance(
             inferencer_name,
             partial(
-                ImageClassificationInferencer,
-                model,
-                classes=text,
-                text_prototype=text))
+                ImageClassificationInferencer, model, classes=text_classes))
+
         result = inferencer(image)[0]['pred_scores'].tolist()
 
         if inferencer.classes is not None:
